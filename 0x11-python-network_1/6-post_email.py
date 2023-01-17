d350bcc998da750e75241f081c7db6c3e3970c93
Oluwafemi <ogundarearnold@gmail.com>
2023-01-17 18:49:41 +0100

    Python Network 1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,6 +10,6 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data = {'email': email} 
-    response = requests.post(url, data) 
+    data = {'email': email}
+    response = requests.post(url, data)
     print(response.text)
